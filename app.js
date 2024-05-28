1fbe025dbb1e49903563cced1126eb25de5dbdbf
Gene Villacian <evillacian@gmail.com>
2024-05-27 23:07:55 -0700

    Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+<div class="app">
+    <div class="header">
+        <header id="header">
+        <nav class="navbar" id="navbar">
+            <div class="navborders">
+            <a class="nav-links" href="#about-me">About Me</a>
+            <a class="nav-links" href="#projects">Projects</a>
+            <a class="nav-links" href="#coding-challenges">Coding Challenges</a>
+            </div>
+        </nav>
+        </header>
+    </div>
+    <div class="main">
+        <main id="main">
+        <section id="background">
+        </section>
+        <section class="center" id="about-me">
+            <div id="about-me" class="navborders">
+                <a id="profile-link" href="https://www.freecodecamp.org/genevill" target="_blank">freeCodeCamp Portfolio</a>
+                <a id="profile-link" href="https://play.google.com/store/apps/dev?id=6083836623150592844" target="_blank">Google Developer Profile</a>
+                <a id="profile-link" href="https://github.com/genevill" target="_blank">GitHub Portfolio</a>
+            </div>
+            <div class="center">
+            <div>
+                <AppButton text="Pomodoro Timer"></AppButton>
+                <AppButton text="Calculator"></AppButton>
+                <AppButton text="Drum App"></AppButton>
+                <AppButton text="Markdown App"></AppButton>
+                <AppButton text="Quote App"></AppButton>
+            </div>
+            </div>
+        </section>
+        <section id="projects">
+            <div class="center" id="projects-center">
+                <Switcher />
+            </div>
+        </section>
+        <section class="center" id="coding-challenges">
+        </section>
+        </main>
+    </div>
+    <footer id="footer">
+    </footer>
+</div>
---
+++ markdown.js
@@ -0,0 +1,53 @@
+import './Markdown.css';
+import { marked } from 'marked';
+import { useEffect } from 'react';
+
+// They don't necessarily need to take props
+// This one also has an explicit return
+export const MarkdownApp = () => {
+
+    const defaultText =
+        "# Hello\n" +
+        "## Hello\n" +
+        "There's also [links](https://www.freecodecamp.org)\n\n" +
+        "> Block Quotes!\n\n" +
+        "`<div></div>`\n" +
+        "```\n" +
+        "// this is multi-line code:\n" +
+        "function anotherExample(firstLine, lastLine) {\n" +
+        "   if (firstLine == '```' && lastLine == '```') {\n" +
+        "      return multiLineCode;\n" +
+        "   }\n" +
+        "}\n" +
+        "```\n" +
+        "- And of course there are lists.\n\n" +
+        "![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)\n\n" +
+        "**Hello**";
+
+    const defaultParsed = marked.parse(defaultText);
+
+    const TextBox = props => {
+        return (<textarea id="markdown-editor" name="markdown-editor" rows="10" onInput={LoadText} defaultValue={defaultText}></textarea>)
+    }
+
+    const PreviewBox = props => {
+        return (<p id="markdown-preview"></p>)
+    }
+
+    function LoadText() {
+        document.getElementById("markdown-preview").innerHTML = marked.parse(document.getElementById("markdown-editor").value);
+    }
+
+    useEffect(LoadText);
+
+    return (
+        <div id="markdown-parent">
+            <div class="markdown-box" id="markdown-box1">
+                <TextBox text={defaultText} />
+            </div>
+            <div class="markdown-box" id="markdown-box2">
+                <PreviewBox />
+            </div>
+        </div>
+    );
+}
